09ceee0c0cb703e68241fe3373a75728f5563dda
vgratian <vgratian@utopianlab.am>
2022-06-05 21:25:06 +0400

    use go's templating lib

--- sharedir.go
+++ sharedir.go
@@ -2,205 +2,205 @@ package main
 
 import (
 	"fmt"
-	"io/fs"
+	"html/template"
 	"io/ioutil"
 	"log"
 	"mime"
 	"net/http"
 	"os"
-	fp "path/filepath"
+	"path/filepath"
 	"strings"
 	"time"
 )
 
-const (
-	logfp = "httpfs.log"
-
-	ic_dir	= ""
-	ic_sdr	= ""
-	ic_arx  = ""
-	ic_aud	= ""
-	ic_cod	= ""
-	ic_vid	= ""
-	ic_img	= ""
-	ic_fil	= ""
-	ic_txt	= ""
-
-	txtHead = `
-<html>
-  <head>
-    <title>sharedir: %s</title>
-	<style type="text/css">
-	table th{ text-align: justify; }
-	table td{ text-align: justify; }
-	</style>
-  </head>
-  <body>
-  <h2 style="color:grey;">index: %s</h2>
-  <table width="80%" style="color:grey;">
-    <thead>
-      <th>Filename</th>
-      <th>Size</th>
-      <th>Modified</th>
-    </thead>
-    <tbody>
-`
-	txtTail = `    </tbody>
-  </table>
-  </body>
-</html>
-`
-	txtFileEntry = `      <tr><td><a href="%s">%s</a></td><td>%d</td><td>%s</td></tr>\n
-`
-	txtDirEntry = `      <tr><td><a href="%s">%s/</a></td><td>-*-</td><td>%s</td></tr>\n
-`
-)
+const templateFp = "template.html"
 
 var (
-	rootDir = ""
-	recursive = false
+	root string				 // root of shared directory
+	recursive bool = false   // recursive mode
+	home string              // home directory of program
 )
 
-//func getIcon(fn string) string { }
+type safePath struct {
+	abs string		// absolute path (unvisible to clients)
+	rel string		// path relative to root (visible)
+}
 
-func getMimeType(fn string) string {
+// Check if the requested path is admissible. If so, return
+// a safePath instance. Path is admissible if it is
+// valid and a subpath of root.
+// TODO handle symlinks and non-regular files.
+func parseSafePath(raw string) *safePath {
 	var (
-		mtype string
-		parts []string
+		sp *safePath
+		err error
 	)
 
-	if parts = strings.Split(fn, "."); len(parts) > 1 {
-		mtype = mime.TypeByExtension("."+parts[len(parts)-1])
+	sp = new(safePath)
+	sp.abs = filepath.Join(root, strings.TrimPrefix(raw, "/"))
+
+	if sp.abs, err = filepath.Abs(sp.abs); err != nil {
+		log.Printf("     absolute path: %v", err)
+		return nil
 	}
 
-	if mtype == "" {
-		return "application/octet-stream"
+	if ! strings.HasPrefix(sp.abs, root) {
+		log.Print("     not in root")
+		return nil
 	}
-	return mtype
+
+	if sp.abs != root {
+		sp.rel = strings.TrimPrefix(sp.abs, root)
+		sp.rel = strings.TrimPrefix(sp.rel, "/")
+	}
+
+	return sp
 }
 
-func serveReject(w http.ResponseWriter, s int, msg string) {
-	w.WriteHeader(s)
-	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
-	w.Write([]byte(msg))
+// Guess MIME-type of file based on file extension.
+// By default assume 'binary data'.
+func guessMimeType(fp string) string {
+	var mimet, ext string
+
+	if ext = filepath.Ext(fp); ext != "" {
+		mimet = mime.TypeByExtension(ext)
+
+		if mimet != "" {
+			return mimet
+		}
+	}
+
+	return "application/octet-stream"
 }
 
+// Main entry point of the HTTP handling pipeline.
+// General logic: client requests a path (file or directory).
+// We check if request is admissable and pass the request on
+// to the other functions.
 func serve(w http.ResponseWriter, r *http.Request) {
 	var (
-		target string
+		sp *safePath
 		err error
-		info os.FileInfo
+		inf os.FileInfo
 	)
 
-	log.Printf("%s - %s - %s", r.Method, r.RemoteAddr, r.RequestURI)
+	log.Printf("%s: %s - %s", r.Method, r.RemoteAddr, r.RequestURI)
 
-	if target, err = fp.Abs(fp.Join(rootDir, r.RequestURI)); err != nil {
-		log.Printf("error parsing path: %s", err.Error())
-		serveReject(w, http.StatusBadRequest, "bad target path")
+	if r.RequestURI == "/~favicon.ico" {
+		serveIcon(w)
 		return
 	}
 
-	if ! strings.HasPrefix(target, rootDir) {
-		log.Printf("target not in root: %s", target)
-		serveReject(w, http.StatusUnauthorized, "unauthorized path")
+	if sp = parseSafePath(r.RequestURI); sp == nil {
+		serveFailure(w, http.StatusBadRequest, "invalid path")
 		return
 	}
 
-
-	// TODO handle symlinks and other non-regular files
-	if info, err = os.Stat(target); err != nil {
-		log.Printf("error stat target: %s", err.Error())
-		serveReject(w, http.StatusNotFound, "target not found")
+	if inf, err = os.Stat(sp.abs); err != nil {
+		log.Printf("     stat target: %v", err)
+		serveFailure(w, http.StatusNotFound, "invalid path")
 		return
 	}
 
-	if ! info.IsDir() {
-		serveFile(w, target)
-		return
+	if inf.IsDir() {
+		serveDir(w, sp)
+	} else {
+		serveFile(w, sp)
 	}
-
-	serveDir(w, target, r.RequestURI)
 }
 
-func serveFile(w http.ResponseWriter, fp string) {
+// Write HTTP-status-code indicating failure and the plain-text
+// error message.
+func serveFailure(w http.ResponseWriter, code int, message string) {
+	w.WriteHeader(code)
+	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+	w.Write([]byte(message))
+}
+
+func serveFile(w http.ResponseWriter, p *safePath) {
 
 	var (
 		err error
-		num int
+		size int
 		data []byte
-		mtype string
 	)
 
-	mtype = getMimeType(fp)
-
-	if data, err = ioutil.ReadFile(fp); err != nil {
-		log.Printf("error read [%s]: %v", fp, err)
-		serveReject(w, http.StatusInternalServerError, "error")
+	if data, err = ioutil.ReadFile(p.abs); err != nil {
+		log.Printf("     read file [%s]: %v", p.abs, err)
+		serveFailure(w, http.StatusInternalServerError, "server error")
 		return
 	}
 
-	if num, err = w.Write(data); err != nil {
-		log.Printf("error write response: %v", err)
-		serveReject(w, http.StatusInternalServerError, "error")
+	if size, err = w.Write(data); err != nil {
+		log.Printf("     write response: %v", err)
+		serveFailure(w, http.StatusInternalServerError, "server error")
 		return
 	}
-	log.Printf("wrote data [%s]: %d", mtype, num)
 
-	//w.WriteHeader(http.StatusOK)
-	w.Header().Set("Content-Type", mtype) //; charset=binary")
+	w.Header().Set("Content-Type", guessMimeType(p.rel))
+	log.Printf("     served %d bytes", size)
 }
 
-func serveDir(w http.ResponseWriter, dir, subdir string) {
+func serveIcon(w http.ResponseWriter) {
+	var p *safePath
 
-	var (
-		err		error
-		entries	[]os.DirEntry
-		e		os.DirEntry
-		info	fs.FileInfo
-		res	string
+	p = new(safePath)
+	p.abs = filepath.Join(home, "sharedir.ico")
+	serveFile(w, p)
+}
+
+func serveDir(w http.ResponseWriter, p *safePath) {
+
+	var	(
+		err	error
+		tmp	*template.Template
 	)
 
-	if entries, err = os.ReadDir(dir); err != nil {
-		log.Fatalf("read fsDir: %s", err.Error())
+	data := struct {
+		DirName string
+		Content []os.DirEntry
+	}{ DirName: "/"+p.rel }
+
+	if data.Content, err = os.ReadDir(p.abs); err != nil {
+		log.Fatalf("read dir: %v", err)
 	}
 
-	res += fmt.Sprintf(txtHead, subdir, subdir)
-	for _, e = range entries {
-		if info, err = e.Info(); err != nil {
-			log.Printf("error: read [%s] info: %s", e.Name(), err.Error())
-			continue
-		}
-		//s += fmt.Sprintf(txtEntry, getIcon(f), f.Name(), i.Size(), i.ModTime().String())
-		if info.IsDir() {
-			res += fmt.Sprintf(txtDirEntry,
-					fp.Join(subdir, e.Name()),
-					e.Name(),
-					info.ModTime().Format(time.UnixDate),
-			)
-		} else {
-			res += fmt.Sprintf(txtFileEntry,
-					fp.Join(subdir, e.Name()),
-					e.Name(), 
-					info.Size(), 
-					info.ModTime().Format(time.UnixDate),
-			)
-		}
-	}
-	res += txtTail
+	tmp, err = template.New(templateFp).Funcs(
+		template.FuncMap{
+			"ttos" : func(t time.Time) string {
+				return t.Format("2006-01-02 15:04:05")
+			},
+			"href" : func(n string) string {
+				if p.rel == "" {
+					return n
+				}
+				return filepath.Join(p.rel, n)
+			},
+		}).ParseFiles(filepath.Join(home, templateFp))
 
-	w.WriteHeader(http.StatusOK)
+	if err != nil {
+		log.Fatalf("parse template: %v", err)
+	}
+
+	if err = tmp.Execute(w, data); err != nil {
+		log.Fatalf("execute template: %v", err)
+	}
+
 	w.Header().Set("Content-Type", "text/html; charset=utf-8") 
-
-	if _, err = w.Write([]byte(res)); err != nil {
-		log.Fatalf("write response: %s", err.Error())
-	}
 }
 
 
-func usage() {
-	fmt.Printf("usage: %s [addr] [directory] [-r]\n", fp.Base(os.Args[0]))
-}
+const usage = `Quickly and safely share content of a directory over HTTP.
 
+Usage: sharedir [-r] [-a ADDR] [directory]
+
+Options and arguments:
+    -r          Recursive mode (also share subdirectories)
+    -a ADDR     Start HTTP server on this address (default: ':2022')
+    directory   Directory to share (default: current directory)
+	
+Report bugs: https://github.com/vgratian/sharedir
+`
 
 func main() {
 
@@ -209,57 +209,77 @@ func main() {
 		srv  http.Server
 		err	 error
 		addr string
-		mode string
 	)
+
+	addr = ":2022"
 	
-	mode = "non-recursive"
 	if len(os.Args) > 1 {
-		for _, a := range os.Args[1:] {
+		a := os.Args[1]
+		if a == "help" || a == "--help" || a == "-h" {
+			fmt.Println(usage)
+			os.Exit(0)
+		}
 
-			if a == "help" || a == "--help" || a == "-h" {
-				usage()
-				os.Exit(0)
-			}
-
+		i := 1
+		for i < len(os.Args) {
+			a = os.Args[i]
 			if a == "-r" {
 				recursive = true
-				mode = "recursive"
-			} else if addr == "" {
-				addr = a
-			} else {
-				rootDir = a
+				i+=1
+				continue
 			}
+
+			if a == "-a" {
+				if i+1 < len(os.Args) {
+					addr = os.Args[i+1]
+					i+=2
+				} else {
+					fmt.Printf("missing argument for '-a'")
+					os.Exit(1)
+				}
+				continue
+			}
+
+			root = os.Args[i]
+			i+=1
 		}
 	}
 
-	if addr != "" && rootDir == "" {
-		rootDir = addr
-	}
-
-	// defaults
-	if addr == "" {
-		addr = ":2022"
-	}
-
-	if rootDir == "" {
-		rootDir = "."
-	}
-
 	// convert to absolute path (helps to make sure we don't share anything outside)
-	if rootDir, err = fp.Abs(rootDir); err != nil {
+	if root, err = filepath.Abs(root); err != nil {
 		fmt.Println(err)
 		os.Exit(1)
 	}
 
-	log.Printf("sharing directory %sly [%s]", mode, rootDir)
+	if recursive {
+		log.Printf("sharing directory [%s] recursively", root)
+	} else {
+		log.Printf("sharing directory [%s]", root)
+	}
+
+	// home directory (where we can load html template and icon from)
+	if home, err = os.Executable(); err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+
+	log.Printf("home from executable: [%s]", home)
+
+	if home, err = filepath.EvalSymlinks(home); err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+
+	home, _ = filepath.Split(home)
+	log.Printf("found home directory [%s]", home)
 
 	mux = http.NewServeMux()
 	mux.HandleFunc("/", serve)
 	srv.Handler = mux
 	srv.Addr = addr
 
-	log.Printf("serving at [%s]", srv.Addr)
+	log.Printf("serving at %s", srv.Addr)
 	if err = srv.ListenAndServe(); err != nil {
-		log.Fatalf("start http service: %s", err.Error())
+		log.Fatalf("starting HTTP service: %s", err.Error())
 	}
 }
